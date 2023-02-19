00205b5d28c37ccace188a85a1e9313ac2c3a765
TyroneLTL <tyrone.stafford@lighthousetechnologies.io>
2023-02-19 17:58:15 +0200

    Tyrone stages outstanding changes

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 
 const router = express.Router();
 
@@ -10,27 +11,47 @@ router.get('/', (req, res) => {
 // POST new payment
 router.post('/', (req, res) => {
     // res.json({ mssg: 'POSTING payment' })
-    //
-        fetch(
-            "https://online.yoco.com/v1/charges/",{
-              headers: {
-                  'X-Auth-Secret-Key': process.env.REACT_APP_YOKO_SK
-                 }
+    const headers = { "X-Auth-Secret-Key": process.env.YOKO_SK }
+    
+    res.status(200).send({
+        body: req.body,
+        headers: headers
+    })
+
+    if(res.status(200)){
+        const fetchData = async() => {
+            const response = await fetch('https://online.yoco.com/v1/charges/', {
+                method: 'POST',
+                body: req.body,
+                headers: headers,
             })
-           .then((result) => {
-             res.status(200).send({
-               result:
-                 "[API] - Successful POST",
-             });
-             return result;
-           })
-           .catch((err) => {
-             res.status(500).send({
-               message:
-                 "Oops, there was a problem: " + err.message,
-             });
-        });
+            const data = await response.json()
+            console.log(data)
+        }
+        fetchData()
+    }
+    }
     //
-    });
+        // fetch(
+        //     "https://online.yoco.com/v1/charges/",{
+        //       headers: {
+        //           'X-Auth-Secret-Key': process.env.REACT_APP_YOKO_SK
+        //          }
+        //     })
+        //    .then((result) => {
+        //      res.status(200).send({
+        //        result:
+        //          "[API] - Successful POST",
+        //      });
+        //      return result;
+        //    })
+        //    .catch((err) => {
+        //      res.status(500).send({
+        //        message:
+        //          "Oops, there was a problem: " + err.message,
+        //      });
+        // });
+    //
+    );
 
 module.exports = router;

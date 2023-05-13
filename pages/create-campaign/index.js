99e39ae181fef871f62425cc6306c96e1e385a70
Tyrone Stafford <undyingdesign@gmail.com>
2023-05-13 09:13:03 +0200

    Updates to create campaign component via dashboard

--- pages/create-campaign/index.js
+++ pages/create-campaign/index.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import { useRouter } from 'next/router';
 
+import { FaAngleLeft } from "react-icons/fa";
 import Image from "next/image";
 import Create from '../../comps/Signup/CreateCampaign';
 import Share from '../../comps/Signup/ShareCampaign';
@@ -11,12 +13,17 @@ import { initializeApp } from 'firebase/app';
 
 
 const CreateCampaign = () => {
+    const router = useRouter();
     const [step, setStep] = useState(1); // 1 = create, 2 = share
     const [uid, setUid] = useState(null);
     
     const app = initializeApp(cfg);
     const db = getFirestore(app);
     const auth = getAuth();
+
+    const navigateToPage = (page) => {
+      router.push(page);
+    }
 
     useEffect(() => {
         if (!uid) {
@@ -33,7 +40,19 @@ const CreateCampaign = () => {
         <div className='flex flex-col justify-between py-10 px-10 h-[100vh]'>
             <Image src="/img/logo-white.svg" alt='chippin logo' className='w-10/12 max-w-[180px] max-h-[47px] mx-auto' width="180" height="47" priority />
 
-            <div className="animate__animated animate__fadeInUp animate__faster bg-white rounded-t-md absolute z-10 w-full md:w-7/12 lg:w-5/12 max-w-[600px] h-[85%] top-[15%] left-0 right-0 mx-auto">
+            <div className="animate__animated animate__fadeInUp animate__faster bg-white rounded-t-md fixed z-10 w-full md:w-7/12 lg:w-5/12 max-w-[600px] h-[85%] top-[15%] left-0 right-0 mx-auto">
+            <div
+              className="transition-all duration-300 flex flex-row justify-between relative -top-20 px-5"
+            >
+              <div
+                className="flex flex-row items-center text-white cursor-pointer"
+                onClick={() => {
+                  navigateToPage('/');
+                }}
+              >
+                <FaAngleLeft className="text-xl" /> <span>back</span>
+              </div>
+            </div>
                 <div>
                     {step === 1 && <Create db={db} nextStep={() => setStep(2)} />}
                     {step === 2 &&
